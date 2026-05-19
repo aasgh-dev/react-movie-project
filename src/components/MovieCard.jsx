fd95839905f95610c60da75cbc000b84fa847692
aasgh <aasgh.devolper@gmail.com>
2026-05-19 18:55:41 +0300

    final commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,21 @@
 import "../css/MovieCard.css";
+import {useMovieContext} from "../contexts/MovieContext.jsx";
 
-function MovieCard({ movie }) {
+function MovieCard({movie}) {
+    const {isFavorites, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite=isFavorites(movie.id)
 
-    function onFavoriteClick() {
-        alert("clicked")
+    function onFavoriteClick(e) {
+        e.preventDefault()
+        if(favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
 
     return <div className="movie-card">
         <div className="movie-poster">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
-                <button className="favorite-btn" onClick={onFavoriteClick} >
+                <button className={`favorite-btn ${favorite?"active":""}`} onClick={onFavoriteClick}>
                     ❤︎
                 </button>
             </div>
